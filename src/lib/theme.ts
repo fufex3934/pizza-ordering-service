84bbbff8ec3a046fde1da2c783d9bddbffadebdb
fufex3934 <fufawakgari174@gmail.com>
2025-05-14 15:09:11 +0300

    pizza ordering

---
+++ src/lib/theme.ts
@@ -0,0 +1,15 @@
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#d32f2f', // Red
+    },
+    secondary: {
+      main: '#ffa000', // Amber
+    },
+  },
+});
+
+export default theme;
